ef7b8cfc03ef5871746d8d8ac8d9ebfbe3aa5126
Dominic Cockman <20927611@ecm-csse2113-l.uniwa.uwa.edu.au>
2014-10-27 20:59:10 +0800

    small changes, fix tomorrow 	modified:   gnatidread.h 	modified:   vStart.glsl

--- project1/LINUX_OR_MINGW/vStart.glsl
+++ project1/LINUX_OR_MINGW/vStart.glsl
@@ -7,6 +7,9 @@ in  vec2 vTexCoord;
 out  vec4 position;
 out  vec3 normal;
 out  vec2 texCoord;
+in ivec4 boneIDs;
+in  vec4 boneWeights;
+uniform mat4 boneTransforms[64];
 
 uniform mat4 ModelView;
 uniform mat4 Projection;
@@ -16,8 +19,10 @@ uniform mat4 Projection;
 
 void main()
 {
+    mat4 boneTransform = 5;
     gl_Position = Projection * ModelView * vPosition;
     texCoord = vTexCoord;
-    normal = vNormal;
-    position = vPosition;
+    normal = vNormal * 3;
+    position = vPosition * 3;
 }
+
